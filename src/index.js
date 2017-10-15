821b726123969727a162c61384baa40ff4d67875
Alejandro Nanez <alejonanez@gmail.com>
2017-10-15 14:28:15 -0500

    Basic api

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 require('dotenv').config();
 
-console.log(process.env.SENDGRID_API);
+const express = require('express');
+const app = express();
+const expressPort = process.env.PORT || 3000;
 
+app.get('/', function handlePost(req, res) {
+  res
+    .status(200)
+    .json({
+      message: 'hello world'
+    });
+});
+
+app.listen(expressPort, function () {
+  console.log(`Running on port ${expressPort}`);
+});
