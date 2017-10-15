c7ca6f0887defa43cf1a748c7c65b2a60794a3d2
Alejandro Nanez <alejonanez@gmail.com>
2017-10-15 14:51:16 -0500

    Send basic emails

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const sgMail = require('@sendgrid/mail');
+
 const app = express();
 const expressPort = process.env.PORT || 3000;
 
+// Sendgrid config
+sgMail.setApiKey(process.env.SENDGRID_API);
+
+// Express config
 app.use(bodyParser.json());
 
+// Express routes
 app.get('/', function handleGet(req, res) {
   res
     .status(200)
@@ -22,15 +29,23 @@ app.post('/send-email', function handlePost(req, res) {
     message
   } = req.body;
 
+  const messageToSend = {
+    to: process.env.TO_EMAIL,
+    from: email,
+    subject: 'Contact from your website!',
+    text: message
+  };
+
+  sgMail.send(messageToSend).then(console.log).catch(console.error);
+
   res
   .status(200)
   .json({
-    name,
-    email,
-    message
+    status: 'OK'
   });
 });
 
+// Start Express
 app.listen(expressPort, function () {
   console.log(`Running on port ${expressPort}`);
 });
