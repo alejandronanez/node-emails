a8f3a2eefead996899a444636d8fdf54128f8e02
Alejandro Nanez <alejonanez@gmail.com>
2017-10-15 14:40:14 -0500

    Receive data from POST

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 require('dotenv').config();
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const expressPort = process.env.PORT || 3000;
 
-app.get('/', function handlePost(req, res) {
+app.use(bodyParser.json());
+
+app.get('/', function handleGet(req, res) {
   res
     .status(200)
     .json({
       message: 'hello world'
     });
+});
+
+app.post('/send-email', function handlePost(req, res) {
+  const {
+    name,
+    email,
+    message
+  } = req.body;
+
+  res
+  .status(200)
+  .json({
+    name,
+    email,
+    message
+  });
 });
 
 app.listen(expressPort, function () {
